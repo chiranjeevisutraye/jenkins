8ee9ca5ff7f228b22e2deee093b28015150c8b55
chiranjeevisutraye <chiranjeevi.sutraye@gmail.com>
2020-08-07 16:51:10 +0530

    added apptests

--- src/main/java/com/chiru/jenkins/App.java
+++ src/main/java/com/chiru/jenkins/App.java
@@ -6,6 +6,9 @@ package com.chiru.jenkins;
  */
 public class App 
 {
+	public String coverage() {
+		return "Hello";
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
